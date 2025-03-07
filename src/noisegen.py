a077e1abf29ff8efa0ce3de3e030a20d117f6f65
Humdaan Mustafa <humishum@gmail.com>
2025-03-07 00:44:53 -0800

    .:

--- src/noisegen.py
+++ src/noisegen.py
@@ -1,11 +1,11 @@
 import numpy as np
 import numpy.typing as npt
 def white_noise(signal:npt.ArrayLike, freq:float):
-    noise = np.random.normal(0, 1, signal.__len__)
+    noise = np.random.normal(0, 1, len(signal))
     noisy_signal = signal + noise * freq
     return noisy_signal
 
 def pink_noise(signal:npt.ArrayLike, freq:float):
-    noise = np.random.randn(signal.__len__)
+    noise = np.random.randn(len(signal))
     pink_noise_signal = signal + (noise / np.arange(1, signal.__len__+1)) * freq
     return pink_noise_signal
